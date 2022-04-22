5a87be15f372aa2ae77fc29947dec6ef56fb4f5f
Kathrin De Cecco <kathrin@dececco.co>
2022-04-22 17:45:10 +0200

    Initial commit

---
+++ core/src/ws/toast/lit/LITGame.java
@@ -0,0 +1,22 @@
+package ws.toast.lit;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class LITGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
---
+++ desktop/src/ws/toast/lit/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ws.toast.lit;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ws.toast.lit.LITGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Lost in Translation");
+		new Lwjgl3Application(new LITGame(), config);
+	}
+}
