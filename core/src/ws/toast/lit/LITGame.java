8ba2d9b62bf058479c531abb5a51d14a42095bab
Kathrin De Cecco <kathrin@dececco.co>
2022-04-22 18:52:55 +0200

    Update LITGame to start into the main title screen

--- core/src/ws/toast/lit/LITGame.java
+++ core/src/ws/toast/lit/LITGame.java
@@ -1,22 +1,38 @@
 package ws.toast.lit;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import lombok.var;
+import com.badlogic.gdx.Game;
+import ws.toast.lit.scenes.MainTitleScreen;
 
-public class LITGame extends ApplicationAdapter {
-	
+public class LITGame extends Game {
+
+	public SpriteBatch batch;
+	public ShapeRenderer shapes;
+	public BitmapFont readableFont;
+	public BitmapFont fantasyFont;
+
 	@Override
 	public void create () {
+		var screen = new MainTitleScreen(this);
+
+		batch = new SpriteBatch();
+		shapes = new ShapeRenderer();
+		readableFont = new BitmapFont();
+		fantasyFont = new BitmapFont();
+
+		shapes.setAutoShapeType(true);
+
+		this.setScreen(screen);
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-	}
-	
-	@Override
 	public void dispose () {
+		batch.dispose();
+		shapes.dispose();
+		readableFont.dispose();
+		fantasyFont.dispose();
 	}
 }
