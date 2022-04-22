094a3c3597503cd35667b2303f90efe9889312b0
Kathrin De Cecco <kathrin@dececco.co>
2022-04-22 18:52:14 +0200

    Add placeholder for ConversationScreen

---
+++ core/src/ws/toast/lit/scenes/ConversationScreen.java
@@ -0,0 +1,20 @@
+package ws.toast.lit.scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ScreenAdapter;
+import ws.toast.lit.LITGame;
+
+public class ConversationScreen extends ScreenAdapter {
+
+    private final LITGame game;
+
+    public ConversationScreen(LITGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+    }
+}
