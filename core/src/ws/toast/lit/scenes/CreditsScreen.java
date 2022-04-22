135ed3916f310e70e14c152f499d5f51f619f5e5
Kathrin De Cecco <kathrin@dececco.co>
2022-04-22 18:52:28 +0200

    Add placeholder for CreditsScreen

---
+++ core/src/ws/toast/lit/scenes/CreditsScreen.java
@@ -0,0 +1,17 @@
+package ws.toast.lit.scenes;
+
+import com.badlogic.gdx.ScreenAdapter;
+import ws.toast.lit.LITGame;
+
+public class CreditsScreen extends ScreenAdapter {
+
+    private final LITGame game;
+
+    public CreditsScreen(LITGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+    }
+}
