bac5896f346aaab84ee3d9a32371091813775514
Kathrin De Cecco <kathrin@dececco.co>
2022-04-23 14:17:20 +0200

    Add fader and jukebox instances to Game class

--- core/src/ws/toast/lit/LITGame.java
+++ core/src/ws/toast/lit/LITGame.java
@@ -1,12 +1,13 @@
 package ws.toast.lit;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import lombok.var;
 import com.badlogic.gdx.Game;
+import ws.toast.lit.audio.Jukebox;
+import ws.toast.lit.graphics.ScreenFader;
 import ws.toast.lit.scenes.MainTitleScreen;
 
 public class LITGame extends Game {
@@ -15,6 +16,8 @@ public class LITGame extends Game {
 	public ShapeRenderer shapes;
 	public BitmapFont readableFont;
 	public BitmapFont fantasyFont;
+	public ScreenFader fader;
+	public Jukebox jukebox;
 
 	@Override
 	public void create() {
@@ -25,6 +28,8 @@ public class LITGame extends Game {
 		shapes = new ShapeRenderer();
 		readableFont = new BitmapFont(adelpheFontFileHandle);
 		fantasyFont = new BitmapFont();
+		fader = new ScreenFader(this);
+		jukebox = new Jukebox(this);
 
 		shapes.setAutoShapeType(true);
 		readableFont.setColor(1.0F, 1.0F, 1.0F, 1.0F);
@@ -33,10 +38,17 @@ public class LITGame extends Game {
 	}
 
 	@Override
+	public void resize(int width, int height) {
+		Gdx.graphics.setWindowedMode(width, height);
+		Gdx.gl.glViewport(0, 0, width, height);
+	}
+
+	@Override
 	public void dispose() {
 		batch.dispose();
 		shapes.dispose();
 		readableFont.dispose();
 		fantasyFont.dispose();
+		jukebox.dispose();
 	}
 }
