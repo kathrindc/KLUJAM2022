f4da2dca20bfefc20ba051bb2bebe4a2860164c7
Kathrin De Cecco <kathrin@dececco.co>
2022-04-24 13:07:40 +0200

    Add first batch of dictionary entries

--- core/src/ws/toast/lit/logic/DictionaryEntry.java
+++ core/src/ws/toast/lit/logic/DictionaryEntry.java
@@ -4,27 +4,17 @@ public class DictionaryEntry {
 
     public static final DictionaryEntry[] ENTRIES = {
             new DictionaryEntry("Hello", "Hallo"),
-            new DictionaryEntry("Egg", "Ei"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule"),
-            new DictionaryEntry("Owl", "Eule")
+            new DictionaryEntry("A word", "How are you"),
+            new DictionaryEntry("Where", "Where are we"),
+            new DictionaryEntry("Acceptable", "Good"),
+            new DictionaryEntry("Something", "Police"),
+            new DictionaryEntry("Oh no", "Bad"),
+            new DictionaryEntry("you did that", "to accuse"),
+            new DictionaryEntry("Gamebert", "Gamebert"),
+            new DictionaryEntry("Animal Place", "Zoo"),
+            new DictionaryEntry("AbCdEf", "Where is..."),
+            new DictionaryEntry("illegal", "Crime"),
+            new DictionaryEntry("Despacito", "Punishment"),
     };
 
     private final String foreignWord;
